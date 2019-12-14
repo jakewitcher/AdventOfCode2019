6683b10de0b0465061bbd53861ad52a3c74d5f66
Jake Witcher <jjwitcher09@gmail.com>
2019-12-13 23:06:20 -0500

    solution to Day6

---
+++ Day6/Solution.fsx
@@ -0,0 +1,106 @@
+open System.IO
+
+type Object = string
+
+type Orbit =
+    | Object of Object
+    | Orbit of Object * Orbit list
+
+module List =
+    let tryHead list =
+        match list with
+        | [] -> None
+        | list' -> List.head list' |> Some
+
+module Orbit =
+    let parse (orbit: string) =
+        let validateOrbit (orbit: string []) =
+            if Array.length orbit = 2 then Some(orbit.[0], orbit.[1])
+            else None
+
+        orbit.Split [| ')' |] |> validateOrbit
+
+    let parseAll =
+        sprintf "%s\\orbits.txt" __SOURCE_DIRECTORY__
+        |> File.ReadAllLines
+        |> Array.choose parse
+        |> Array.toList
+
+    let findCenter orbits =
+        orbits
+        |> List.unzip
+        |> fun (centers, orbits) -> Set.difference (Set.ofList centers) (Set.ofList orbits)
+        |> Set.toList
+        |> List.tryHead
+
+    let rec findOrbitting orbitsRaw object =
+        let findOrbittingOfOrbits orbit =
+            match orbit with
+            | Object obj -> findOrbitting orbitsRaw obj
+            | Orbit(cent, orb) -> Orbit(cent, orb)
+
+        let findOrbits orbits =
+            match orbits with
+            | [] -> object |> Object
+            | orbits' -> (object, orbits' |> List.map findOrbittingOfOrbits) |> Orbit
+
+        orbitsRaw
+        |> List.filter (fun orbit -> fst orbit = object)
+        |> List.map (snd >> Object)
+        |> findOrbits
+
+    let findAll orbits =
+        orbits
+        |> findCenter
+        |> Option.map (findOrbitting orbits)
+        |> Option.defaultValue ("no orbits Found" |> Object)
+
+    let length orbit =
+        let rec length' total orbit =
+            match orbit with
+            | Object _ -> total
+            | Orbit(_, orbits) -> total + List.sumBy (length' (total + 1)) orbits
+
+        length' 0 orbit
+
+    let pathTo object orbit =
+        let rec pathTo' path object orbit =
+            match orbit with
+            | Object obj ->
+                if obj = object then obj :: path
+                else []
+            | Orbit(center, orbits) ->
+                if center = object then center :: path
+                else orbits |> List.collect (pathTo' (center :: path) object)
+
+        pathTo' [] object orbit |> List.rev
+
+    let distance object1 object2 orbit =
+        let lastCommonNode path1 path2 =
+            Set.intersect (Set.ofList path1) (Set.ofList path2)
+            |> Set.toList
+            |> List.length
+            |> fun distance -> distance - 1
+
+        let pathFromCommon path common =
+            path
+            |> List.skip common
+            |> Set.ofList
+
+        (orbit |> pathTo object1, orbit |> pathTo object2)
+        |> fun (path1, path2) ->
+            lastCommonNode path1 path2 |> pathFromCommon path1, lastCommonNode path1 path2 |> pathFromCommon path2
+        |> fun (path1, path2) ->
+            Set.union path1 path2
+            |> Set.toSeq
+            |> Seq.windowed 2
+            |> Seq.length
+
+
+Orbit.parseAll
+|> Orbit.findAll
+|> Orbit.length
+
+Orbit.parseAll
+|> Orbit.findAll
+|> Orbit.distance "SAN" "YOU"
